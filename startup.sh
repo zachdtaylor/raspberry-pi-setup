e12d98839760ed69bb34ed1cccead1fff7b04a85
Zach Taylor <zachtylr04@gmail.com>
2020-05-24 19:05:42 -0500

    Create startup script

---
+++ startup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update && sudo-apt-get upgrade
+
+# Install zsh
+sudo apt-get install zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
