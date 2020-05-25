34e6ff143e96d7a311fd397a510a7ddeebe6879d
Zach Taylor <zachtylr04@gmail.com>
2020-05-24 19:33:22 -0500

    Add vim install

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -8,5 +8,8 @@ chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# Install vim
+sudo apt install vim
+
 # Symlink files
 ./link.sh
